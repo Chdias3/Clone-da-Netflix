233dedc10e7ea566ed41af7bc29d2e560597af81
Charles Alexsander <chdias30@outlook.com>
2022-05-12 20:03:27 -0300

    :tada: first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# sabedoriaFlix :collision:
+
+SabedoriaFlix - front- end https://chdias3.github.io/sabedoriaFlix/
+
+![image](https://user-images.githubusercontent.com/91853449/164166983-5706b1cc-1377-4aa2-8b59-fe5033dc1735.png)
+
+![image](https://user-images.githubusercontent.com/91853449/164167202-dd66f095-fa44-4f34-80fd-00fd862f5e8d.png)
+
+![image](https://user-images.githubusercontent.com/91853449/164167697-72902bd6-79b5-481b-b554-23a7f54ae7ee.png)
---
+++ js/main.js
@@ -0,0 +1,159 @@
+$('.owl-carousel').owlCarousel({
+  loop: true /*para deixar a rolagem infinita*/,
+  margin: 10 /*margin entre um filme e outro*/,
+  nav: false,
+  /*bolinhas de navegação*/ responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 3
+    },
+    1000: {
+      items: 5
+    }
+  }
+})
+
+const modal = {
+  open() {
+    //abrir modal
+    //adicionar a class active ao modal
+    document.querySelector('.modal-overlay').classList.add('active')
+  },
+  close() {
+    //fechar o modal
+    //remover a class active do modal
+    document.querySelector('.modal-overlay').classList.remove('active')
+  }
+}
+
+const modal2 = {
+  open() {
+    //abrir modal
+    //adicionar a class active ao modal
+    document.querySelector('.modal-overlay2').classList.add('active')
+  },
+  close() {
+    //fechar o modal
+    //remover a class active do modal
+    document.querySelector('.modal-overlay2').classList.remove('active')
+  }
+}
+
+const modal3 = {
+  open() {
+    //abrir modal
+    //adicionar a class active ao modal
+    document.querySelector('.modal-overlay3').classList.add('active')
+  },
+  close() {
+    //fechar o modal
+    //remover a class active do modal
+    document.querySelector('.modal-overlay3').classList.remove('active')
+  }
+}
+
+const modal4 = {
+  open() {
+    //abrir modal
+    //adicionar a class active ao modal
+    document.querySelector('.modal-overlay4').classList.add('active')
+  },
+  close() {
+    //fechar o modal
+    //remover a class active do modal
+    document.querySelector('.modal-overlay4').classList.remove('active')
+  }
+}
+
+const modal5 = {
+  open() {
+    //abrir modal
+    //adicionar a class active ao modal
+    document.querySelector('.modal-overlay5').classList.add('active')
+  },
+  close() {
+    //fechar o modal
+    //remover a class active do modal
+    document.querySelector('.modal-overlay5').classList.remove('active')
+  }
+}
+
+const modal6 = {
+  open() {
+    //abrir modal
+    //adicionar a class active ao modal
+    document.querySelector('.modal-overlay6').classList.add('active')
+  },
+  close() {
+    //fechar o modal
+    //remover a class active do modal
+    document.querySelector('.modal-overlay6').classList.remove('active')
+  }
+}
+
+const modal7 = {
+  open() {
+    //abrir modal
+    //adicionar a class active ao modal
+    document.querySelector('.modal-overlay7').classList.add('active')
+  },
+  close() {
+    //fechar o modal
+    //remover a class active do modal
+    document.querySelector('.modal-overlay7').classList.remove('active')
+  }
+}
+
+const modal8 = {
+  open() {
+    //abrir modal
+    //adicionar a class active ao modal
+    document.querySelector('.modal-overlay8').classList.add('active')
+  },
+  close() {
+    //fechar o modal
+    //remover a class active do modal
+    document.querySelector('.modal-overlay8').classList.remove('active')
+  }
+}
+
+const modal9 = {
+  open() {
+    //abrir modal
+    //adicionar a class active ao modal
+    document.querySelector('.modal-overlay9').classList.add('active')
+  },
+  close() {
+    //fechar o modal
+    //remover a class active do modal
+    document.querySelector('.modal-overlay9').classList.remove('active')
+  }
+}
+
+const modal10 = {
+  open() {
+    //abrir modal
+    //adicionar a class active ao modal
+    document.querySelector('.modal-overlay10').classList.add('active')
+  },
+  close() {
+    //fechar o modal
+    //remover a class active do modal
+    document.querySelector('.modal-overlay10').classList.remove('active')
+  }
+}
+
+const modal11 = {
+  open() {
+    //abrir modal
+    //adicionar a class active ao modal
+    document.querySelector('.modal-overlay11').classList.add('active')
+  },
+  close() {
+    //fechar o modal
+    //remover a class active do modal
+    document.querySelector('.modal-overlay11').classList.remove('active')
+  }
+}
